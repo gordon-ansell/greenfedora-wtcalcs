d0cf635401b39aa94d5320b4899962d442c136fb
GordonAnsell <moving@ga1964.com>
2021-04-20 15:27:11 +0100

    Dev

--- src/Application/WTCalcsApplication.php
+++ src/Application/WTCalcsApplication.php
@@ -11,9 +11,6 @@ namespace WTCalcs\Application;
 
 use GreenFedora\Application\AbstractHttpApplication;
 use GreenFedora\Application\ApplicationInterface;
-//use GreenFedora\DependencyInjection\ContainerInterface;
-
-//use GreenFedora\DependencyInjection\Container;
 use GreenFedora\Config\Config;
 use GreenFedora\Locale\Locale;
 use GreenFedora\Logger\Logger;
@@ -91,47 +88,6 @@ class WTCalcsApplication extends AbstractHttpApplication implements ApplicationI
 
 		return $di;
 
-		// =================================
-
-		/*
-		// Load up the service manager.
-		$container = new Container();
-
-		// Config, locale.
-		$container->create(Config::class, [], 'config')->process($env);
-		$container->create(Locale::class, [$container->get('config')->locale], 'locale');
-
-		// Logger.
-		$lcfg = $container->get('config')->logger;
-		$formatter = new StdLogFormatter($lcfg);
-		$writers = array(new FileLogWriter($lcfg, $formatter));
-		if ('prod' != $env) {
-			$writers[] = new ForcedConsoleLogWriter($lcfg, $formatter);		
-		}
-		$container->create(Logger::class, [$lcfg, $writers], 'logger');
-
-		// Lang, inflector.
-		$container->create(Lang::class, [$container->get('locale')->getLangCode()], 'lang');
-		$container->create(Inflector::class, [], 'inflector');
-
-		//Session.
-		$container->create(Session::class, [$container->get('config')->session], 'session');
-
-		// Router.
-		$container->create(Router::class, [$container->get('config')->routing, $container], 'router');
-
-		// Template.
-		$tplType = $container->get('config')->templateType;
-		if ('plates' == $tplType) {
-			$container->create(PlatesTemplate::class, [$container->get('config')->template, $container], 'template');
-		} else if ('smarty' == $tplType) {
-			$container->create(SmartyTemplate::class, [$container->get('config')->template, $container], 'template');
-		} else {
-			throw new \InvalidArgumentException(sprintf("No template support for type '%s'", $tplType));
-		}
-
-		return $container;
-		*/
 	}
 
 	/**
